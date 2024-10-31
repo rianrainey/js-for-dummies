8e48a0477a937b6eab30d9606dc389a40b04e102
Rian Rainey <rianrainey@gmail.com>
2024-10-31 06:38:51 -0400

    Call fake server during onMounted()

--- chpt4-vue/vue-project/src/views/TodoView.vue
+++ chpt4-vue/vue-project/src/views/TodoView.vue
@@ -1,30 +1,54 @@
 <script setup>
-  import { reactive } from 'vue'; // For state mgmt
+  import { reactive, onMounted } from 'vue'; // For state mgmt
   const state = reactive({
     todos: [],
-    newTodo: '',
   })
 
-  function addTodo() {
-    state.todos.push({
-      title: state.newTodo,
-      done: false
-    })
-    state.newTodo = ''
-  }
+  onMounted(() => {
+    // fetch is included by default. Don't need axios dependency.
+    fetch('https://jsonplaceholder.typicode.com/todos') // returns a promise
+      .then(response => {
+        const json = response.json()
+        console.log('Response: ', json)
+        return json // promise returns Response object. We just want the json
+      })
+      .then(data => {
+        console.log('data: ', data);
+        return state.todos = data // takes json and assigns it to the todos array in our state object
+      })
+      .catch (error => {
+        console.error('Error fetching todos:', error);
+      })
+
+      // try {
+      //   let response = await fetch('https://jsonplaceholder.typicode.com/todos');
+      //   const todos = await response.json();
+      //   state.todos = todos;
+      // } catch (error) {
+      //   console.log("Error fetching data: ", error);
+      // }
+      // try {
+      //   let response = await fetch('http://localhost:3000/todos', {
+      //     mode: 'cors'
+      //   })
+      //   const todos = await response.json()
+      //   state.todos = todos
+      // } catch (error) {
+      //   console.log(error);
+      // })
+  })
 </script>
 
 <template>
   <div>
-    <h1>To-Do List</h1>
+    <h1>Todo List</h1>
     <ul>
       <li v-for="todo in state.todos">
-        <input type="checkbox" v-model="todo.done" />
-        <span v-text="todo.title"></span>
+        <input type="checkbox" v-model="todo.completed" />
+        &nbsp;<span v-text="todo.id"></span>
+        &nbsp;<span v-text="todo.title"></span>
       </li>
     </ul>
-    <input type="text" v-model="state.newTodo" placeholder="Add new task" />
-    <button @click="addTodo">Add Todo</button>
   </div>
 </template>
 
