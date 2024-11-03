8734b9514063d9f567f55b428e0f28a9a3319570
Rian Rainey <rianrainey@gmail.com>
2024-11-03 06:46:17 -0500

    Done learning about ref and reactivity

--- chpt4-vue/vue-project/src/views/TemplateView.vue
+++ chpt4-vue/vue-project/src/views/TemplateView.vue
@@ -1,13 +1,20 @@
 <script setup>
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
 // const props = defineProps(['firstName', 'lastName']) // don't have to assign this to a local object if I don't want to
 defineProps(['firstName', 'lastName'])
 
 const state2 = reactive({
   cupsOfCoffee: 0,
 })
+
+const refCoffee = ref(0)
+
 function drinkCoffee() {
   state2.cupsOfCoffee++
+}
+
+function drinkRefCoffee() {
+  refCoffee.value++
 }
 
 const state = reactive({
@@ -21,7 +28,8 @@ const state = reactive({
     <h3>By {{ state.authorName }}</h3>
     <h1>{{ firstName }} {{ lastName }}</h1>
     <h3>Coffee consumed: {{ state2.cupsOfCoffee }}</h3>
+    <h3>Coffee consumed: {{ refCoffee }}</h3>
     <button @click="drinkCoffee">Drink Coffee</button>
-    <button v-on:click="drinkCoffee">Drink Coffee</button>
+    <button v-on:click="drinkRefCoffee">Drink Coffee</button>
   </div>
 </template>
